005180e54867b968f90311c129b7065ebe2a1bb1
Guven Karanfil <guven.karanfil@smartface.io>
2024-06-11 18:01:51 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core'
+import { execSync } from 'child_process'
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    execSync(`npm install -g @appcircle/cli`, { stdio: 'inherit' })
+    const accessToken = core.getInput('accessToken')
+    const profileID = core.getInput('profileID')
+    const appPath = core.getInput('appPath')
+    const message = core.getInput('message')
+    console.log('AC_TOKEN:', accessToken)
+    console.log('profileID:', profileID)
+    console.log('appPath:', appPath)
+    console.log('message:', message)
+
+    execSync(`appcircle --version`, { stdio: 'inherit' })
+    execSync(`appcircle login --pat=${accessToken}`, { stdio: 'inherit' })
+    execSync(
+      `appcircle testing-distribution upload --app=${appPath} --distProfileId=${profileID} --message "${message}"`,
+      { stdio: 'inherit' }
+    )
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
